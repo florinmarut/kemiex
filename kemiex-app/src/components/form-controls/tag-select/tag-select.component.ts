19e2741232f08bcaf02989333963fd91057fd9fd
florin.marut99 <florin.marut99@e-uvt.ro>
2024-07-14 17:14:49 +0300

    Create tag select component

---
+++ kemiex-app/src/components/form-controls/tag-select/tag-select.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tag-select',
+  standalone: true,
+  imports: [],
+  templateUrl: './tag-select.component.html',
+  styleUrl: './tag-select.component.scss'
+})
+export class TagSelectComponent {
+
+}
