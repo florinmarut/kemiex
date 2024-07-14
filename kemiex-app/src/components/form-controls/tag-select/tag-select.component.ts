9ceea9ff208870f94f1a09440bd98043ff107fbe
florin.marut99 <florin.marut99@e-uvt.ro>
2024-07-14 17:16:42 +0300

    Update selector of tag select component

--- kemiex-app/src/components/form-controls/tag-select/tag-select.component.ts
+++ kemiex-app/src/components/form-controls/tag-select/tag-select.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'tag-select',
+  selector: 'app-tag-select',
   standalone: true,
   imports: [],
   templateUrl: './tag-select.component.html',
